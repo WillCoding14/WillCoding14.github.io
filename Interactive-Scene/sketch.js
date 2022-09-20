530148aeb61b7d5ec62b1350391c7b57b1630d82
Will <william.hlady@gmail.com>
2022-09-20 12:16:47 -0600

    Progress 9/20

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -5,10 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let charX = 30;
+let charY = 30;
+let startX = windowWidth / 2;
+let startY = windowHeight - windowHeight;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  character();
+}
+
+function character() {
+  rect(startX, startY, charX, charY);
 }
