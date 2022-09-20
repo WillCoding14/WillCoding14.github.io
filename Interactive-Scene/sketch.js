1b13e3292e36f1a796630757a05a99228ac16806
Will <william.hlady@gmail.com>
2022-09-20 11:56:58 -0600

    Test

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -1,10 +1,9 @@
-// Project Title
-// Your Name
-// Date
+// Interactive-Scene
+// Will
+// September 20, 2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
