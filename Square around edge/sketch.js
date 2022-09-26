426d7412d818c6198285803cfabe997962633166
Will <114016408+WillCoding14@users.noreply.github.com>
2022-09-26 11:20:28 -0600

    Add files via upload

---
+++ Square around edge/sketch.js
@@ -0,0 +1,15 @@
+// Square around edge
+// Will
+// Sept 26
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
