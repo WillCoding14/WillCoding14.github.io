dfee31d0dea5b02c870c3b5cc1227ae5f8b69588
Will <114016408+WillCoding14@users.noreply.github.com>
2022-09-26 16:10:40 -0600

    defined basic things

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -5,21 +5,23 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+let x, y, rectHeight, rectWidth;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  x = 0;
+  y = 0;
+  rectHeight = 150;
+  rectWidth = 150;
 }
-
 
 function draw() {
   background(220);
-  character();
+  drawRectangle();
 }
 
-function character() {
-  let startX = width / 2;
-  let startY = height - characterHeight;
-  let characterWidth = width / 10;
-  let characterHeight = height / 10;
-  rect(startX, startY, characterWidth, characterHeight);
-  fill("black");            
+function drawRectangle(){
+  rect(width/2 - rectWidth/2, height-rectHeight, rectWidth, rectHeight)
+  Fill("black")
 }
