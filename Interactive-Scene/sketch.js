ec1a33096354dfbe5b8dafb597dd34f63d188209
Will <william.hlady@gmail.com>
2022-09-29 11:09:49 -0600

    Testing

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -5,16 +5,22 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
-
+let spr;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
-
 function draw() {
-  background(220);
+  background(50);
+  drawSpr;
+}
+function mousePressed() {
+  spr.position.x = mouseX;
+  spr.position.y = mouseY;
 }
 
-
-
+function drawSpr(){
+  spr = rect(width/2, height/2, 40, 40);
+  spr.shapeColor = color(255);
+  spr.velocity.y = 0.5;
+}
 
