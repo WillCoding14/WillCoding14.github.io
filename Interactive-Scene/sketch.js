7c8072ace8cc6066d5d906d15ed9c4890dbfddf4
Will <william.hlady@gmail.com>
2022-09-29 11:15:59 -0600

    restarted

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -5,22 +5,12 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let spr;
+
 function setup() {
   createCanvas(400, 400);
 }
 function draw() {
   background(50);
-  drawSpr;
-}
-function mousePressed() {
-  spr.position.x = mouseX;
-  spr.position.y = mouseY;
-}
 
-function drawSpr(){
-  spr = rect(width/2, height/2, 40, 40);
-  spr.shapeColor = color(255);
-  spr.velocity.y = 0.5;
 }
 
