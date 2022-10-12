c6982513727aea6e99b53818f8d8b9d8ca46298b
Will <william.hlady@gmail.com>
2022-10-12 12:17:55 -0600

    blahblah

--- Interactive-Scene/sketch.js
+++ Interactive-Scene/sketch.js
@@ -7,8 +7,10 @@
 
 
 let state = "start";
-let mainBg, startBg;
+let mainBg, startBg, targetDummy, uzi1, uzi2, uzi3;
 let bX, bY, bW, bH;
+let tLength, uLength;
+let frameLength;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,6 +18,8 @@ function setup() {
   bY = height / 2 - 75;
   bW = 300;
   bH = 150;
+  tLength = 300, uLength = 350;
+  frameLength = 100;
 }
 
 function draw() {
@@ -23,14 +27,22 @@ function draw() {
   if (state === "start") {
     startScreen();
   }
-  if (state === "main") {
+  if (state === "main"){
     image(mainBg, 0, 0, width, height);
+    drawDummies();
+    drawGunMain();
+  }
+  if (state === "shoot") {
+    shootGun();
   }
 }
 
 function mousePressed() {
   if (state === "start" && mouseInBox(bX, bX + 300, bY, bY + 150)) {
     state = "main";
+  }
+  else if (state === "main") {
+    state === "shoot";
   }
 }
 
@@ -51,4 +63,24 @@ function mouseInBox(left, right, top, bottom) {
 
 function preload() {
   mainBg = loadImage("background.png");
+  targetDummy = loadImage("target.png");
+  uzi1 = loadImage("tile000.png");
+  uzi2 = loadImage("tile001.png");
+  uzi3 = loadImage("tile002.png");
+}
+
+function drawGunMain(){
+  image(uzi1, mouseX, height - uLength + 50, uLength, uLength);
+}
+
+function drawDummies(){
+  image(targetDummy, width / 2, height - tLength, tLength, tLength);
+  image(targetDummy, width / 2 - tLength, height - tLength, tLength, tLength);
+  image(targetDummy, width / 2 - tLength * 2, height - tLength, tLength, tLength);
+  image(targetDummy, width / 2 + tLength, height - tLength, tLength, tLength);
+}
+
+
+function shootGun(){
+  state = "main";
 }
