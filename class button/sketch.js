753adba255e99e14190e601e7f69b202538ad6e9
Will <william.hlady@gmail.com>
2022-11-21 11:27:07 -0600

    butt

--- class button/sketch.js
+++ class button/sketch.js
@@ -5,6 +5,29 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let state = "start";
+let theButtons = [];
+
+class Button{
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = "green";
+  }
+
+  display() {
+    rectMode(CENTER);
+    fill(this.color);
+    noStroke();
+    rect(this.x, this.y, this.w, this.h);
+  }
+
+  interact(){
+
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +35,12 @@ function setup() {
 
 function draw() {
   background(220);
+  let b = new Button(width/2, height*0.75, 200, 100);
+  let p = new Button(width/2, height*0.25, 200, 100);
+  theButtons.push(b);
+  theButtons.push(p);
+  for (let i = 0; i < theButtons.length; i++){
+    theButtons[i].display();
+  }
+  
 }
